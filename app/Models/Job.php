1625117149edf37ab6c5ad4deb8639e384fa4b90
Connor Dawson <c.dawson@digital-velocity.co.uk>
2024-12-02 10:51:01 +0000

    if no job found 404

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -30,7 +30,13 @@ class Job
 
     public static function find(int $id): array
     {
-       return Arr::first(static::all(), fn($job) => $job['id'] == $id);
+       $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
+
+       if( !$job){
+           abort(404);
+       }else {
+           return $job;
+       }
     }
 
 }
