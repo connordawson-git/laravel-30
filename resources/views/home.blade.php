c3848a4755fc8f0e387d24ae05212c5478c8444b
Connor Dawson <c.dawson@digital-velocity.co.uk>
2024-12-02 10:04:01 +0000

    example job list lopped through on frotnend, data passed from route

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,12 @@
         Home Page
     </x-slot:heading>
     <h1>Hello from home</h1>
+    <ul>
+        @foreach($jobs as $job)
+            <li>
+                {{ $job['title'] . " " .  $job['salary']}}
+            </li>
+        @endforeach
+    </ul>
+
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'jobs' => [
+            [
+                'title' => 'director',
+                'salary' => '£100,000'
+            ],
+            [
+                'title' => 'programmer',
+                'salary' => '£50,000'
+            ],
+            [
+                'title' => 'teacher',
+                'salary' => '£30,000'
+            ]
+        ]
+    ]);
 });
 Route::get('/about', function () {
     return view('about');
